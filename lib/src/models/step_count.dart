fc83e0429d9edb5120307071fa92111f579ceb6b
SANGWOO PARK <prophet0915@gmail.com>
2024-12-16 19:38:15 +0900

    convert datetime format to ISO string

--- lib/src/models/step_count.dart
+++ lib/src/models/step_count.dart
@@ -9,11 +9,18 @@ class DangpleStepCount {
     required this.timeStamp,
   });
 
+  factory DangpleStepCount.fromJson(Map<String, dynamic> json) {
+    return DangpleStepCount(
+      steps: json['steps'],
+      timeStamp: DateTime.tryParse(json['time_stamp']) ?? DateTime.now(),
+    );
+  }
+
   Map<String, dynamic> toJson() {
     final data = <String, dynamic>{};
 
     data['steps'] = steps;
-    data['time_stamp'] = timeStamp;
+    data['time_stamp'] = timeStamp.toIso8601String();
 
     return data;
   }
